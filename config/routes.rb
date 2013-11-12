930173f3beb058cc85bd3ede9e177e059579a20a
jangelserver <jeangel16@ucatolica.edu.co>
2013-11-11 23:43:59 -0500

    Commit Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Taller::Application.routes.draw do
+  resources :comments
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
